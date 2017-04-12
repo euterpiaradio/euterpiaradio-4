25db62704563cdc0dada89c2ff6055fa5f17a8ce
Yannick Mauray <yannick.mauray@gmail.com>
2017-04-12 17:08:19 +0200

    Pied de page

--- footer.php
+++ footer.php
@@ -3,7 +3,13 @@
     </div>
     <div class="footer-outter">
         <div class="footer-inner">
-        <div>footer</div>
+            <div class="searchbox"><?php get_search_form(); ?></div>
+            <div class="license">
+                <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Licence Creative Commons" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png"/></a>
+                <div xmlns:dct="http://purl.org/dc/terms/" href="http://purl.org/dc/dcmitype/Sound" property="dct:title" rel="dct:type">Le podcast Euterpia Radio</span> est mis à disposition selon les termes de la licence Creative Commons</div>
+                <div><a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">Attribution - Pas d’Utilisation Commerciale - Partage dans les Mêmes Conditions</a>.</div>
+                <div class="cms"><a href="http://www.wordpress.org">Ce site utilise WordPress.</a></div>
+            </div>
         </div>
     </div>
     <?php wp_footer(); ?>
